6ac033b5be29bb775b556f5b60779080f067f8c2
Syahroni Bawafi <syahronibwf@gmail.com>
2025-01-24 15:50:35 +0700

    feat: add banner ui

---
+++ src/components/Banner.tsx
@@ -0,0 +1,9 @@
+const Banner = () => {
+  return (
+    <div className="w-full bg-banner text-white text-sm font-bold flex justify-center items-center py-3">
+      <span>New here? Save 20% with code: YR24</span>
+    </div>
+  );
+};
+
+export default Banner;
