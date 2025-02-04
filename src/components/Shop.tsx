dcc979ed57ff2d438014984de2cbc0c898738421
Syahroni Bawafi <syahronibwf@gmail.com>
2025-02-04 12:47:24 +0700

    fix: fix style shop

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import React from "react";
+import ShopCard from "./ShopCard";
 
 const Shop = () => {
   return (
@@ -11,27 +12,19 @@ const Shop = () => {
           WHAT’S TRENDING
         </button>
       </div>
-      <div className="my-20">
-        <div className="max-w-sm rounded overflow-hidden bg-white p-4 w-[270px]">
-          <div className="relative w-full h-[250px]">
-            <Image
-              src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/eaf524f7-a9f7-4f70-a438-1b0480eb2540/NIKE+COURT+VISION+LO.png"
-              alt="Nike Court Vision Lo"
-              layout="fill"
-              objectFit="cover"
-              unoptimized={true}
-              className="rounded"
-            />
-          </div>
-          <div className="font-bold text-xl mb-2 mt-4">
-            Nike Court Vision Lo
-          </div>
-          <p className="text-gray-700 text-base">$160</p>
-          <div className="flex items-center mt-2">
-            <span className="text-yellow-500 text-xl">******</span>
-            <span className="text-gray-600 ml-2">(88)</span>
-          </div>
-        </div>
+      <div className="grid grid-cols-5">
+        <ShopCard />
+        <ShopCard />
+        <ShopCard />
+        <ShopCard />
+        <ShopCard />
+        <ShopCard />
+        <ShopCard />
+        <ShopCard />
+        <ShopCard />
+        <ShopCard />
+        <ShopCard />
+        <ShopCard />
       </div>
     </div>
   );
